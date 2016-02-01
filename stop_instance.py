b9741e96aaf15be1067ff8675f1416875862cc95
nnakayama1016 <n.nakayama@uchida.co.jp>
2016-02-01 20:46:06 +0900

    first commit

---
+++ stop_instance.py
@@ -0,0 +1,18 @@
+import boto3
+
+INSTANCE_ID = 'i-********'
+client = boto3.client('ec2')
+
+def lambda_handler(event, context):
+    details = client.describe_instances(
+        InstanceIds=[
+            INSTANCE_ID,
+        ]
+    )
+    if details['Reservations'][0]['Instances'][0]['State']['Name'] == 'running':
+        response = client.stop_instances(
+            InstanceIds=[
+                INSTANCE_ID,
+            ]
+        )
+        print response
