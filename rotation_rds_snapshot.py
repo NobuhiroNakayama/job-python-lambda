7c3c46064391281f81d5882d82a257d6ce7d7ef0
nnakayama1016 <n.nakayama@uchida.co.jp>
2016-04-08 19:58:41 +0900

    20160408

--- rotation_rds_snapshot.py
+++ rotation_rds_snapshot.py
@@ -4,18 +4,15 @@ from datetime import datetime, timedelta
 def _(cmd):
     return commands.getoutput(cmd)
 
-def lambda_handler(event, context):
-    instance_name = event['Name']
+def delete_snapshots(instance_name):
     snapshots = boto3.client('rds').describe_db_snapshots(DBInstanceIdentifier=instance_name,SnapshotType='manual')['DBSnapshots']
-
-    print snapshots
     
     for snapshot in snapshots:
-        if int(datetime.now().strftime('%s')) - 2592000 > int(snapshot["SnapshotCreateTime"].strftime('%s')): # If snapshot created time is older than 30 days ago
+        if int(datetime.now().strftime('%s')) - 2592000 > int(snapshot["SnapshotCreateTime"].strftime('%s')): # If snapshot created time is older than 30 days ago, delete snapshot
             # print "delete"
-            # 2592000
-            response = boto3.client('rds').delete_db_snapshot(DBSnapshotIdentifier=snapshot['DBSnapshotIdentifier'])
-        else:
-            # print "not delete"
-            break
+            # 2592000 sec = 30 day
+            response = boto3.client('rds').delete_db_snapshot(DBSnapshotIdentifier=snapshot['DBSnapshotIdentifier'])    
 
+def lambda_handler(event, context):
+    delete_snapshots(event['Name'])
+    
